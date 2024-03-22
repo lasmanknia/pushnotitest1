11d9dca0e992cd6f5b1e46e17b4c0dc006b40b76
Lasman Knia <lasmanknia@gmail.com>
2024-03-22 22:06:07 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const checkPermission = () => {
+  if (!("serviceWorker" in navigator)) {
+    throw new Error("No support");
+  }
+  if (!("Notification" in window)) {
+    throw new Error("error noti");
+  }
+};
+
+const registerSw = async () => {
+  try {
+    const registration = await navigator.serviceWorker.register("sw.js");
+    return registration;
+  } catch (error) {
+    console.error("Service worker registration failed:", error);
+    // You can handle the error here, such as showing a user-friendly message
+    // or performing fallback behavior.
+  }
+};
+
+const requestnotificationPermission = async () => {
+  const permission = await Notification.requestPermission();
+  if (permission !== "granted") {
+    throw new Error("noti not work");
+  } else {
+    new Notification("Hello world");
+  }
+};
+
+try {
+  checkPermission();
+  registerSw();
+  requestnotificationPermission();
+} catch (error) {
+  console.error("Error during service worker setup:", error);
+  // You can handle the error here, such as showing a user-friendly message
+  // or performing fallback behavior.
+}
